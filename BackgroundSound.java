72215b971f2905f496f0da55aaf40a85f4c070c6
fishyCoding <sebastianbalestri@gmail.com>
2026-04-28 18:24:05 -0400

    a

--- BackgroundSound.java
+++ BackgroundSound.java
@@ -12,18 +12,14 @@ public class BackgroundSound extends Sound {
 
     private static final float AMBIENT_STRENGTH = 30f;
 
-    private final Submarine listener;
 
     public BackgroundSound(Submarine listener) {
         super(listener.getX(), listener.getY(), AMBIENT_STRENGTH, "ambient_ocean");
-        this.listener = listener;
     }
 
-    /** Stay glued to the listener so distance falloff is always ~zero. */
     @Override
     public void tick() {
-        x        = listener.getX();
-        y        = listener.getY();
+
         strength = AMBIENT_STRENGTH;
     }
 
